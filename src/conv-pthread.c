8cc0ceb85bdf53026d1f4839352d9a44b2cba98f
roberta1 <roberta1@tcd.ie>
2026-04-01 11:52:45 +0100

    Removed unused code

--- src/conv-pthread.c
+++ src/conv-pthread.c
@@ -64,23 +64,3 @@ void student_conv_pthreads(float *** image, int16_t **** kernels, float *** outp
   threadpool_join(pool);
   threadpool_dealloc(pool);
 }
-
-/*
-int h, w, x, y, c, m;
-
-  for ( m = 0; m < nkernels; m++ ) {
-    for ( w = 0; w < width; w++ ) {
-      for ( h = 0; h < height; h++ ) {
-        double sum = 0.0;
-        for ( c = 0; c < nchannels; c++ ) {
-          for ( x = 0; x < kernel_order; x++) {
-            for ( y = 0; y < kernel_order; y++ ) {
-              sum += image[w+x][h+y][c] * kernels[m][c][x][y];
-            }
-          }
-          output[m][w][h] = (float) sum;
-        }
-      }
-    }
-  }
-*/
