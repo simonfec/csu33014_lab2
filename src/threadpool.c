2e119baafc2cc5fa4d523aa64bd044fd1bc3cb76
roberta1 <roberta1@tcd.ie>
2026-04-03 22:16:06 +0100

    Added better comments

--- src/threadpool.c
+++ src/threadpool.c
@@ -22,6 +22,7 @@ Threadpool* threadpool_alloc(unsigned int nthreads, unsigned int work_buf_size) 
     assert(nthreads > 0);
     assert(work_buf_size > 0);
 
+    // Allocate memory for all fields of the thread pool
     Threadpool* pool = malloc(sizeof(Threadpool));
     if (pool == NULL) return NULL;
 
@@ -39,16 +40,13 @@ Threadpool* threadpool_alloc(unsigned int nthreads, unsigned int work_buf_size) 
         return NULL;
     }
 
+    // Create all worker threads
     for (int i = 0; i < nthreads; i++) {
-        pthread_t thread;
-        int ret = pthread_create(&thread, NULL, worker_routine, (void*)pool->work_stack);
-        // Couldn't create all the required threads
+        int ret = pthread_create(&pool->workers[i], NULL, worker_routine, (void*)pool->work_stack);
         if (ret != 0) {
-            printf("Failed to create thread %d\n", ret);
             threadpool_dealloc(pool);
             return NULL;
         }
-        pool->workers[i] = thread;
     }
     return pool;
 }
@@ -65,6 +63,7 @@ void threadpool_join(Threadpool* pool) {
     workstack_no_more_work(pool->work_stack);
     for (int i = 0; i < pool->nworkers; i++)
         pthread_join(pool->workers[i], NULL);
+    
     threadpool_dealloc(pool);
 }
 
--- src/work_stack.c
+++ src/work_stack.c
@@ -6,15 +6,25 @@
 #include <stdlib.h>
 
 WorkStack* workstack_alloc(unsigned int stack_size) {
+    // Allocate memory
     WorkStack* stack = malloc(sizeof(WorkStack));
     if (stack == NULL) return NULL;
-    stack->stack_size = stack_size;
+
     stack->stack = malloc(stack_size * sizeof(TPoolWork));
+    if (stack->stack == NULL) {
+        workstack_dealloc(stack);
+        return NULL;
+    }
+
+    // Initialise stack params, cond vars, and mutex
+    stack->stack_size = stack_size;
     stack->stack_next = 0;
+
+    // Always returns 0 so no error checking needed
     pthread_mutex_init(&stack->stack_lock, NULL);
     pthread_cond_init(&stack->work_popped, NULL);
     pthread_cond_init(&stack->work_pushed, NULL);
-    if (stack->stack == NULL) return NULL;
+
     return stack;
 }
 
@@ -35,9 +45,12 @@ void workstack_push(WorkStack* stack, TPoolWork work) {
     while (stack->stack_next >= stack->stack_size) {
         pthread_cond_wait(&stack->work_popped, &stack->stack_lock);
     }
+
+    // Push work onto the stack now we know there's space
     stack->stack[stack->stack_next] = work;
     stack->stack_next++;
 
+    // Signal one waiter that there's now work it can get
     pthread_cond_signal(&stack->work_pushed);
     pthread_mutex_unlock(&stack->stack_lock);
 }
@@ -54,9 +67,12 @@ bool workstack_pop(WorkStack* stack, TPoolWork* work_out) {
         }
         pthread_cond_wait(&stack->work_pushed, &stack->stack_lock);
     }
+    // Pop work off the stack now we know there is some work there
     stack->stack_next--;
     *work_out = stack->stack[stack->stack_next];
 
+    // Signal that work's been popped in case the stack was full and a thread is waiting
+    // to push more work
     pthread_cond_signal(&stack->work_popped);
     pthread_mutex_unlock(&stack->stack_lock);
     return true;
@@ -67,7 +83,7 @@ void workstack_no_more_work(WorkStack* stack) {
     stack->no_more_work = true;
     // Signal to all sleeping workers to wake up so they realise no more work's coming.
     // All workers that are already running will realise there's no more work when
-    // they try to wait for more work next
+    // they next try to wait for more work
     pthread_cond_broadcast(&stack->work_pushed);
     pthread_mutex_unlock(&stack->stack_lock);
 }
